030a05c1484cb4bc5c84bf380d30cb083f204f67
JaiJaveria <jaijaveria16@gmail.com>
2021-04-15 17:38:30 +0530

    danation one passed

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -210,6 +210,16 @@ lock_acquire (struct lock *lock)
   ASSERT (!intr_context ());
   ASSERT (!lock_held_by_current_thread (lock));
 
+  //priority donation
+  if (lock->holder !=NULL)
+  {
+    if ( (lock->holder)->priority<thread_current()->priority)
+    {
+      (lock->holder)->priority=thread_current()->priority;
+      // list_sort(&ready_list,&compare_priority,NULL);
+    }
+    /* code */
+  }
   sema_down (&lock->semaphore);
 
   lock->holder = thread_current ();
@@ -246,6 +256,7 @@ lock_release (struct lock *lock)
   ASSERT (lock != NULL);
   ASSERT (lock_held_by_current_thread (lock));
 
+  lock->holder->priority=lock->holder->first_priority;
   lock->holder = NULL;
   sema_up (&lock->semaphore);
 }
